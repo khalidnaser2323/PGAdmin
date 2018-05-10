2005455083bd3da9668e7ae66c70a2c7e4d34255
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-05-10 16:25:37 +0200

    [Technical] Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ChildCardComponent } from './pages/child-card/child-card.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    ChildCardComponent
+    ChildCardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
