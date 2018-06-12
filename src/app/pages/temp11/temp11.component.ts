1aae6047554efd6667ae0aa7fac0da755ec073f5
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-06-12 15:25:46 +0200

    [Technical] Template 11 implementation

--- src/app/pages/temp11/temp11.component.ts
+++ src/app/pages/temp11/temp11.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temp11.component.css']
 })
 export class Temp11Component implements OnInit {
+  activePanel: string = "1";
 
-  constructor() { }
+  tempModel: Temp11;
+  constructor() {
+    this.tempModel = {
+      colOneHeader: "",
+      colOneText: "",
+      colTwoHeader: "",
+      colTwoText: "",
+      colThreeHeader: "",
+      colThreeText: "",
+      colFourText: "",
+      colFiveText: "",
+      colSixText: "",
+      colSevenText: "",
+      colEightText: "",
+      colNineText: "",
+      colTenText: "",
+      colElevenText: "",
+      colTwelveText: ""
+    }
+  }
 
   ngOnInit() {
   }
-
+  showPanel(id: string) {
+    console.log("id to show:  " + id);
+    this.activePanel = id;
+  }
+  saveAll() {
+    console.log("Saved temp model");
+    console.log(this.tempModel);
+  }
 }
